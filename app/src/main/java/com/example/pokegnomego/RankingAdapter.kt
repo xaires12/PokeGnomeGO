5d15762b2ec37350ce08f955b932eb9c4fc86a81
PK127001 <pak.127.0.0.1@gmail.com>
2024-06-14 13:07:31 +0200

    Ranking Adapter class.

---
+++ app/src/main/java/com/example/pokegnomego/RankingAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.pokegnomego
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pokegnomego.com.example.pokegnomego.api.UserService
+import com.example.pokegnomego.databinding.ItemUserBinding
+
+class RankingAdapter(private val users: List<User>) : RecyclerView.Adapter<RankingAdapter.UserViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val binding = ItemUserBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return UserViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        val user = users[position]
+        holder.bind(user)
+    }
+
+    override fun getItemCount() = users.size
+
+    class UserViewHolder(private val binding: ItemUserBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(user: User) {
+            binding.loginRankingTextView.text = user.login
+            binding.pointsTextView.text = user.visit_count.toString()
+        }
+    }
+}
